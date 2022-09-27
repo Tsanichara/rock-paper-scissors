cfb66b6ee34a6c90e4d14b198546752cdb778593
Tyler Sanichara <Tsanichara67@gmail.com>
2022-09-27 14:37:59 -0400

    page now updates with score and results after each round

--- rps.js
+++ rps.js
@@ -26,30 +26,102 @@ let playRound = (playerSelection, computerSelection) => {
     let cs = computerSelection;
 
     if(ps === "rock" && cs === "rock"){
-        return "You both picked Rock. It's a draw."
+        return ["You both picked Rock. It's a draw.", "tie"]
     } else if (ps === "rock" && cs === "paper"){
-        return "Paper beats Rock. You Lose!"
+        return ["Paper beats Rock. You Lose!", "cwin"]
     } else if (ps === "rock" && cs === "scissors"){
-        return "Rock beats Scissors! You Win!"
+        return ["Rock beats Scissors! You Win!", "pwin"]
     } else if (ps === "paper" && cs === "rock") {
-        return "Paper beats Rock! You Win!"
+        return ["Paper beats Rock! You Win!", "pwin"]
 
     } else if (ps === "paper" && cs === "paper") {
-        return "You both picked paper. It's a draw."
+        return ["You both picked paper. It's a draw.", 'tie']
         
     } else if (ps === "paper" && cs === "scissors") {
-        return "Scissors beats Paper. You Lose!"
+        return ["Scissors beats Paper. You Lose!", "cwin"]
         
     } else if (ps === "scissors" && cs === "rock") {
-        return "Rock beats Scissors. You Lose!"
+        return ["Rock beats Scissors. You Lose!", "cwin"]
         
     } else if (ps === "scissors" && cs === "paper") {
-        return "Scissors beats Paper! You Win!"
+        return ["Scissors beats Paper! You Win!", "pwin"]
         
     } else if (ps === "scissors" && cs === "scissors") {
-        return "You both picked Scissors. It's a draw."
+        return ["You both picked Scissors. It's a draw.", "tie"]
         
     }
 
+}
+
+
+let pPoints = document.querySelector(".pPoints");
+let cPoints = document.querySelector(".cPoints");
+
+let roundResult = document.querySelector(".outcome");
+
+let end = document.querySelector(".end");
+
+
+
+let playerScore = 0;
+let computerScore = 0;
+    
+
+let game = (pc) => {
+
+    if(playerScore === 5 || computerScore === 5){
+        return; 
+    }
+
+    let gameOutcome = "";
+
+
+    let result = playRound(pc, getComputerChoice()); 
+
+    if(result[1] === "pwin"){
+        playerScore += 1;
+        
+    } else if(result[1] === "cwin"){
+        computerScore += 1
+            
+    }
+
+    
+
+    pPoints.textContent = `Player Score: ${playerScore}`;
+    cPoints.textContent = `Computer Score: ${computerScore}`;
+
+    roundResult.textContent = result[0];
+
+    if(playerScore === 5 || computerScore === 5){
+
+        if (playerScore > computerScore){
+            gameOutcome = "You have defeated the computer!";
+        } else if (computerScore > playerScore){
+            gameOutcome = "The computer has defeated you!";
+        }
+
+        end.textContent = gameOutcome;
+    }
+        
+
+        
+
+
 
 }
+
+
+document.getElementById("rock").addEventListener("click", function(){
+    game('rock');
+});
+
+document.getElementById("paper").addEventListener("click", function(){
+    game('paper');
+});
+document.getElementById("scissors").addEventListener("click", function(){
+    game('scissors');
+});
+
+
+
